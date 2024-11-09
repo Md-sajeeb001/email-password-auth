ac0303f8a36ceedebfb64dc0dfed1161e4c8189c
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-11-10 01:34:45 +0800

    added sign in form

--- src/components/LogIn/Login.jsx
+++ src/components/LogIn/Login.jsx
@@ -1,24 +1,36 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../FireBaseInit/FireBaseinit";
 import { useState } from "react";
-
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 const Login = () => {
-
   const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const [showPassword, setShowPassword] = useState(false);
 
   const handelSubmitForm = (e) => {
     e.preventDefault();
 
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password);
+    const terms = e.target.terms.checked;
 
-    setErrorMessage('')
+    console.log(email, password, terms);
+
+    setErrorMessage("");
+    setSuccess(false);
+
+    if (!terms) {
+      setErrorMessage("Please accept terms and condition");
+      return;
+    }
 
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         console.log(result);
-
+        setSuccess(result.user);
       })
       .catch((error) => {
         setErrorMessage(error.message);
@@ -48,21 +60,35 @@ const Login = () => {
                 required
               />
             </div>
-            <div className="form-control">
+            <div className="form-control relative">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
               />
+              <button
+                onClick={() => setShowPassword(!showPassword)}
+                className=" absolute right-4 top-14"
+              >
+                {showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}
+              </button>
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
                 </a>
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="label justify-start gap-2 cursor-pointer">
+                <input type="checkbox" name="terms" className="checkbox" />
+                <span className="label-text">
+                  accept our terms and condition
+                </span>
               </label>
             </div>
             <div className="form-control mt-6">
@@ -71,6 +97,17 @@ const Login = () => {
           </form>
 
           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          {success && (
+            <p className="text-green-500">
+              user email: add successful {success.email}
+            </p>
+          )}
+          <p>
+            new to the website ? please
+            <Link className="text-white" to="/singin">
+              Sing in
+            </Link>
+          </p>
         </div>
       </div>
     </div>
--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,61 +1,88 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../FireBaseInit/FireBaseinit";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Signin = () => {
+  const [error, setError] = useState("");
+
+  const [success, setSuccess] = useState(false);
+
+  const handelSingInSubmit = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    // reset
+    setSuccess(false);
+    setError("");
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+        setSuccess(result.user);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+        setError(error.message);
+      });
+  };
+
   return (
-    <div>
-      <form method="POST">
-        <div className="grid grid-cols-2 gap-6 ">
-          <div className="space-y-3">
-            <label>Name:</label> <br />
-            <input
-              name="name"
-              type="text"
-              placeholder="your name"
-              className="input input-bordered w-full"
-              autoComplete="off"
-            />
-          </div>
-          <div className="space-y-3">
-            <label>Email:</label> <br />
-            <input
-              name="email"
-              type="text"
-              placeholder="your email"
-              className="input input-bordered w-full"
-              autoComplete="off"
-            />
-          </div>
-          <div className="space-y-3">
-            <label>Password:</label> <br />
-            <input
-              name="password"
-              type="text"
-              placeholder="password"
-              className="input input-bordered w-full"
-              autoComplete="off"
-            />
-          </div>
-          <div className="space-y-3">
-            <label>Address:</label> <br />
-            <input
-              name="address"
-              type="text"
-              placeholder="your address"
-              className="input input-bordered w-full"
-              autoComplete="off"
-            />
-          </div>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Sing in now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-        <div className="pt-14">
-          <textarea
-            name="message"
-            autoComplete="off"
-            placeholder="message"
-            className="textarea textarea-bordered textarea-lg w-full "
-          ></textarea>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handelSingInSubmit} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+          {error && <p className="text-red-500">{error}</p>}
+          {success && (
+            <p className="text-green-500">user sing is successfully</p>
+          )}
+
+          <p>already have an account? please <Link className="text-white" to="/login">Log in</Link></p>
         </div>
-        <div className="pt-6 text-center">
-          <button className="btn">Sign in</button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
