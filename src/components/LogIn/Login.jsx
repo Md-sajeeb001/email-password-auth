b7f545cba6c76e66aa6e2c900cf9681eb1214b3d
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-11-09 22:03:03 +0800

    added log in user with email and password

--- src/components/LogIn/Login.jsx
+++ src/components/LogIn/Login.jsx
@@ -1,61 +1,78 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../FireBaseInit/FireBaseinit";
+import { useState } from "react";
+
 const Login = () => {
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handelSubmitForm = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+    setErrorMessage('')
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        console.log(errorMessage.message);
+      });
+  };
+
   return (
-    <div>
-      <form method="POST">
-        <div className="grid grid-cols-2 gap-6 ">
-          <div className="space-y-3">
-            <label>Name:</label> <br />
-            <input
-              name="name"
-              type="text"
-              placeholder="your name"
-              className="input input-bordered w-full"
-              autoComplete="off"
-            />
-          </div>
-          <div className="space-y-3">
-            <label>Email:</label> <br />
-            <input
-              name="email"
-              type="text"
-              placeholder="your email"
-              className="input input-bordered w-full"
-              autoComplete="off"
-            />
-          </div>
-          <div className="space-y-3">
-            <label>Password:</label> <br />
-            <input
-              name="password"
-              type="text"
-              placeholder="password"
-              className="input input-bordered w-full"
-              autoComplete="off"
-            />
-          </div>
-          <div className="space-y-3">
-            <label>Address:</label> <br />
-            <input
-              name="address"
-              type="text"
-              placeholder="your address"
-              className="input input-bordered w-full"
-              autoComplete="off"
-            />
-          </div>
+    <div className="hero bg-base-200 w-full">
+      <div
+        className="hero-content flex-col w-[50%] lg:flex-row-reverse
+       "
+      >
+        <div className="card bg-base-100 w-full shadow-2xl">
+          <h1 className="text-5xl text-center font-bold pt-8">Login now!</h1>
+
+          <form onSubmit={handelSubmitForm} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         </div>
-        <div className="pt-14">
-          <textarea
-            name="message"
-            autoComplete="off"
-            placeholder="message"
-            className="textarea textarea-bordered textarea-lg w-full "
-          ></textarea>
-        </div>
-        <div className="pt-6 text-center">
-          <button className="btn">Log in</button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
